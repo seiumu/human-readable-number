a41d154fce626224b391df1bbc9291fbe47588d0
Serghei Musaji <serghei.musaji@gmail.com>
2022-01-05 23:05:09 -0400

    fix: task solved

--- src/index.js
+++ src/index.js
@@ -1,78 +1,69 @@
 module.exports = function toReadable(number) {
-  if (number <= 20) {
-    console.log(upTo20[number]);
-    return upTo20[number];
-  } else {
-    var numtext = number.toString();
-    var digits = number.toString().length;
-    var a = Number(numtext[digits - 3]);
-    var b = Number(numtext[digits - 2]);
-    var c = Number(numtext[digits - 1]);
-    if (isNaN(a)) {
-      a = "";
+  const upTo20 = [
+      "zero",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eightteen",
+      "ninetineen",
+      "twenty",
+  ];
+  const tens = [
+      "ten",
+      "twenty",
+      "thirty",
+      "fourty",
+      "fifty",
+      "sixty",
+      "seventy",
+      "eighty",
+      "ninety",
+  ];
+
+    if (number <= 20) {
+        console.log(upTo20[number]);
+        return upTo20[number];
     } else {
-      a = first(a) + " hundred ";
+        var numtext = number.toString();
+        var digits = number.toString().length;
+        var a = Number(numtext[digits - 3]);
+        var b = Number(numtext[digits - 2]);
+        var c = Number(numtext[digits - 1]);
+        if (isNaN(a)) {
+            a = "";
+        } else {
+            a = upTo20[a] + " hundred ";
+        }
+
+        if (b === 0 || isNaN(b)) {
+            b = "";
+        } else {
+            b = tens[b-1] + " ";
+        }
+
+        if (c === 0) {
+            c = "";
+            b = b.slice(0, -1);
+        } else {
+            c = upTo20[c];
+        }
+
+        var result = a + b + c;
+        console.log(result);
+        return result;
     }
-
-    if (b === 0 || isNaN(b)) {
-      b = "";
-    } else {
-      b = second(b - 1) + " ";
-    }
-
-    if (c === 0) {
-      c = "";
-    } else {
-      c = third(c);
-    }
-
-    var result = a + b + c;
-    console.log(result);
-    return result;
-  }
-};
-
-const upTo20 = [
-  "zero",
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-  "ten",
-  "eleven",
-  "twelve",
-  "thirteen",
-  "fourteen",
-  "fifteen",
-  "sixteen",
-  "twenty",
-];
-const tens = [
-  "ten",
-  "twenty",
-  "thirty",
-  "fourty",
-  "fifty",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-];
-
-function first(a) {
-  return upTo20[a];
 }
-function second(b) {
-  return tens[b];
-}
-function third(c) {
-  return upTo20[c];
-}
-
-toReadable(99);
